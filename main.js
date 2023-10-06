d846af1d820dfd921af67bd3f4cbc1d8ed60571c
Sohan Reza <sohanreja22@gmail.com>
2023-10-06 18:53:23 +0600

    Unstable release 1: checkFinished function added to check if the game is finished or not!

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
+"use strict";
+
 const boxes=Array.from(document.querySelectorAll(".diagonal"));
-const restart=document.querySelector("#btn")
+
+let restart=document.querySelector("#btn");
+const box = document.getElementById("main-box");
 const X_text="X";
 const O_text="O";
+let attempt=0;
 let currentPlayer=X_text;
 let check=Array(9).fill(null);
 
 const startGame=()=>{
   boxes.forEach((box)=>{
-    box.addEventListener('click',boxclicked)
+    box.addEventListener('click', boxclicked)
   });
 }
 
 function boxclicked(e){
+  attempt++;
   let boxId=e.target.id;
   if(check[boxId]===null){
     check[boxId]=currentPlayer;
@@ -23,13 +29,66 @@ function boxclicked(e){
       currentPlayer=X_text;
     }
   }
+  checkFinished();
 }
+
+
+function checkFinished() {
+  let win=false;
+ 
+  const log = console.log;
+
+  //check horizontal
+  if ((check[0] !== null && check[1] !== null && check[2] !== null) && (check[0] === check[1] && check[1] === check[2])) {
+    box.innerHTML +=`<span id="first-vertical"></span>`;
+    win=true;
+  }else if((check[3] !== null && check[4] !== null && check[5]!== null) && (check[3]===check[4] && check[4]===check[5])) {
+    box.innerHTML +=`<span id="second-vertical"></span>`;
+    win=true;
+  }else if((check[6] !== null && check[7] !== null && check[8]!== null) && (check[6]===check[7] && check[7]===check[8])) {
+    box.innerHTML +=`<span id="third-vertical"></span>`;
+    win=true;
+  }
+
+  //check vertically
+  if((check[0] !== null && check[3] !== null && check[6]!== null) && (check[0]===check[3] && check[3]===check[6])) {
+    box.innerHTML +=`<span id="first-horizontal"></span>`;
+    win=true;
+  }else if((check[1] !== null && check[4] !== null && check[7]!== null) && (check[1]===check[4] && check[4]===check[7])) {
+    box.innerHTML +=`<span id="second-horizontal"></span>`;
+    win=true;
+  }else if((check[2] !== null && check[5] !== null && check[8]!== null) && (check[2]===check[5] && check[5]===check[8])) {
+    box.innerHTML +=`<span id="third-horizontal"></span>`;
+    win=true;
+  }
+
+  //check diagonally
+  if((check[0] !== null && check[4] !== null && check[8]!== null) && (check[0]===check[4] && check[4]===check[8])) {
+    box.innerHTML +=`<span id="negative-diagonal"></span>`;
+    win=true;
+  }else if((check[2] !== null && check[4] !== null && check[6]!== null) && (check[2]===check[4] && check[4]===check[6])) {
+    box.innerHTML +=`<span id="positive-diagonal"></span>`;
+    win=true;
+  }
+
+  if(win) {
+    alert(`Player ${(currentPlayer === "X") ? "O" : "X"} Won!`);
+    location.reload();
+  }else{
+    if(attempt === 9) {
+      alert("Draw");
+      location.reload();
+    }
+  }
+
+}
+
 
 function Restart(){
   check.fill(null);
   boxes.forEach((box)=>{
     box.innerText='';
-  })
+  });
 }
 restart.addEventListener('click',Restart);
 
